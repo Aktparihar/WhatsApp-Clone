a6fdb06671a83c3f4993b872c78437c208732a1a
Aktparihar <ankitpariharmiku@gmail.com>
2020-12-30 00:38:21 +0530

    SideBar has been created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDK1c3GO3cmydKgUJnfhQ-dYcs-6CnqFdk",
+    authDomain: "whatsapp-clone-38b76.firebaseapp.com",
+    projectId: "whatsapp-clone-38b76",
+    storageBucket: "whatsapp-clone-38b76.appspot.com",
+    messagingSenderId: "804450482272",
+    appId: "1:804450482272:web:b0b1c3e2489f705303eb05"
+  };
