1b279a5f6b69f31b77e8ec5f2914886cc17f5084
Aktparihar <ankitpariharmiku@gmail.com>
2020-12-31 03:47:06 +0530

    added new chat adding option

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDK1c3GO3cmydKgUJnfhQ-dYcs-6CnqFdk",
     authDomain: "whatsapp-clone-38b76.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "804450482272",
     appId: "1:804450482272:web:b0b1c3e2489f705303eb05"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth,provider };
+export default db;
