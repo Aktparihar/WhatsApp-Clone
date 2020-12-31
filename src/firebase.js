d275e22d9bcc35252f46fbe24c4c9dbff54c5612
Aktparihar <ankitpariharmiku@gmail.com>
2020-12-31 11:47:25 +0530

    updated gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDK1c3GO3cmydKgUJnfhQ-dYcs-6CnqFdk",
-    authDomain: "whatsapp-clone-38b76.firebaseapp.com",
-    projectId: "whatsapp-clone-38b76",
-    storageBucket: "whatsapp-clone-38b76.appspot.com",
-    messagingSenderId: "804450482272",
-    appId: "1:804450482272:web:b0b1c3e2489f705303eb05"
-  };
+import { firebaseConfig } from './firebaseConfig.js';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
